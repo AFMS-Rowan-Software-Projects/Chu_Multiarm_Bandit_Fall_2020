ff4aa2fcb946f4ee118c2fa6252d70d21de1fc99
KeaneRowan <52941383+KeaneRowan@users.noreply.github.com>
2020-11-03 10:55:49 -0500

    Add files via upload

--- DecisionMaker.java
+++ DecisionMaker.java
@@ -1,66 +1,21 @@
-public class DecisionMaker {
+public abstract class DecisionMaker {
 
-    Channel[] simulatedNetwork;
-
-    public DecisionMaker(Channel[] simulatedNetwork){
-        this.simulatedNetwork = simulatedNetwork;
-    }
-
-    public int bruteForce(int pingsPerChannel, int numTrials){
-
-        int numSuccesses = 0;
-		int numPings = 0;
-
-        int currChannel = 0;
-		int currNumPings = 0;
-		int currNumSuccess = 0;
-		int bestChannel = 0;
-        double bestRate = 0.0;
-        for (int i = 0; i < numTrials; i++)
-		{
-			// Exploration Phase
-			if (currChannel < simulatedNetwork.length)
-			{
-				// Pings the current channel, performs appropriate record-keeping
-				if (simulatedNetwork[currChannel].ping())
-				{
-					numSuccesses++;
-					currNumSuccess++;
-				}
-				currNumPings++;
-				
-				// Switches to the next channel if we reach pingsPerChannel, performs appropriate record-keeping
-				if (currNumPings == pingsPerChannel)
-				{
-					double currRate = (double) currNumSuccess / pingsPerChannel;
-					
-					// Debug Info
-					System.out.println("Channel " + currChannel + "\tEstimated Success Rate: " + currRate);
-					
-					if (currRate > bestRate)
-					{
-						bestRate = currRate;
-						bestChannel = currChannel;
-					}
-					
-					currChannel++;
-					currNumPings = 0; 
-					currNumSuccess = 0;
-				}
-			}
-			// Exploitation Phase
-			else
-			{
-				if (simulatedNetwork[bestChannel].ping())
-				{
-					numSuccesses++;
-				}
-			}
-			
-			numPings++;
-		}
-        System.out.println("Brute Force Rate: " + numSuccesses/numPings);
-        return bestChannel;
-    }
-    
+	protected NetworkManager network;
+	
+	public DecisionMaker(NetworkManager network)
+	{
+		this.network = network;
+	}
+	
+	/**
+	 * Chooses the next channel to ping, and returns whether it was successful.
+	 * @return
+	 */
+	public abstract boolean pingChannel();
+	
+	/**
+	 * Returns the fields of the DecisionMaker object.
+	 * @return
+	 */
+	public abstract Object[] getResults();
 }
