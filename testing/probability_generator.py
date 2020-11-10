f1011de35c4f4a2d29f233a247b3f95399aa09f2
M Sarosh Khan <msk15163@gmail.com>
2020-11-10 13:06:15 -0500

    Add files via upload
    Fixed formatting.

--- testing/probability_generator.py
+++ testing/probability_generator.py
@@ -1,10 +1,15 @@
 import random;
 
 def generate_data(filename, numChannels, x_bar_success, x_bar_failure, std):
-    f = open(filename, "w")
+    fi = open(filename, "w")
     for i in range(numChannels):
-        f.write(str(x_bar_success + random.uniform(-1*std, std)) + "," + str(x_bar_failure+random.uniform(-1*std, std)))
-    f.close()
+        s = x_bar_success + random.uniform(-1*std, std)
+        f = x_bar_failure + random.uniform(-1*std, std)
+        if(i != numChannels-1):
+            fi.write(str(s) + "," + str(f) + ",")
+        else:
+            fi.write(str(s) + "," + str(f))
+    fi.close()
     return f
 
 #generate_data("test1.csv", 100, 96, 65, 10)
@@ -12,14 +17,14 @@ def generate_data(filename, numChannels, x_bar_success, x_bar_failure, std):
 def generate_sequential_data():
     points = 32
     while points <= 1024:
-        s = 60
-        f = 60
-        std = random.uniform(0, 5)
-        while s<=90:
-            f_name = str(points) + "_" + str(s) + "_" + str(f)
+        s = .60
+        f = .60
+        std = random.uniform(0, .05)
+        while s<=.90:
+            f_name = str(points) + "_" + str(round(s, 2)) + "_" + str(round(f, 2))
             generate_data(f_name, points, s, f, std)
-            s += 5
-            f += 4
+            s += .05
+            f += .04
         points = points << 1
 
 generate_sequential_data()
